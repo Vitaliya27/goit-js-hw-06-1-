b8a316c517a2e80d35cdfd258d7210470cfe7e0f
Vitaliya27 <vitality.prohorenko@gmail.com>
2024-03-11 22:10:58 +0200

    completed task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.value;
+
+  if (amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100.');
+    return;
+  }
+
+const boxes = [];
+for (let i = 0; i < amount; i++) {
+  const box = document.createElement('div');
+  const size = 30 + i * 10;
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  boxes.push(box);
+}
+
+  boxesContainer.append(...boxes);
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
